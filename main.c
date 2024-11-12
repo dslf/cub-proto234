1a555ed10717b3798c8e76088484512e3292037b
Denis Lafin <dslf@live.ru>
2024-11-13 02:22:45 +0300

    tables

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
+#include "wchar.h"
 #include <stdio.h>
+
+#define wg 1
+#define wr 2
+#define wb 4
+#define wo 8
+
+#define gr 16
+#define go 32
+#define ob 64
+#define br 128 
+
+#define yg 256
+#define yo 512
+#define yb 1024
+#define yr 2048
 
 int main() {
 
+printf("%d\n", yg);
 return 0;
 
 }
