59a88bd026f738b0230c709805c61bde5810d220
Denis Lafin <dslf@live.ru>
2024-11-15 06:30:45 +0300

    L move matrix

--- c_move_matrix.c
+++ c_move_matrix.c
@@ -15,12 +15,18 @@ void rotateSide(int direction, int *cubeArr) {
             cubeArr[MAPPING_TOP]=buffer[MAPPING_FRONT];
             cubeArr[MAPPING_TOP+3]=buffer[MAPPING_FRONT+3];
             cubeArr[MAPPING_TOP+6]=buffer[MAPPING_FRONT+6];
-
+            /* TOP -> BACK */
+            cubeArr[MAPPING_BACK]=buffer[MAPPING_TOP+6];
+            cubeArr[MAPPING_BACK+3]=buffer[MAPPING_TOP+3];
+            cubeArr[MAPPING_BACK+6]=buffer[MAPPING_TOP];
+            /* BACK -> BOTTOM */
+            cubeArr[MAPPING_BOTTOM]=buffer[MAPPING_BACK+6];
+            cubeArr[MAPPING_BOTTOM+3]=buffer[MAPPING_BACK+3];
+            cubeArr[MAPPING_BOTTOM+6]=buffer[MAPPING_BACK];
             /* BOTTOM -> FRONT*/
             cubeArr[MAPPING_FRONT]=buffer[MAPPING_BOTTOM];
             cubeArr[MAPPING_FRONT+3]=buffer[MAPPING_BOTTOM+3];
             cubeArr[MAPPING_FRONT+6]=buffer[MAPPING_BOTTOM+6];
-
             break;
     }
     /* printf("do we free?\n"); */
