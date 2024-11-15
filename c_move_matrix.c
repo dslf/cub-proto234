0d0f7c0e269d7ff07f2640d206df22bd2e7e8a36
Denis Lafin <dslf@live.ru>
2024-11-15 06:43:29 +0300

    added left face ratation

--- c_move_matrix.c
+++ c_move_matrix.c
@@ -27,6 +27,17 @@ void rotateSide(int direction, int *cubeArr) {
             cubeArr[MAPPING_FRONT]=buffer[MAPPING_BOTTOM];
             cubeArr[MAPPING_FRONT+3]=buffer[MAPPING_BOTTOM+3];
             cubeArr[MAPPING_FRONT+6]=buffer[MAPPING_BOTTOM+6];
+            /* LEFT SIDE ROTATION, TOP 3 */
+            cubeArr[MAPPING_LEFT]=buffer[MAPPING_LEFT+2];
+            cubeArr[MAPPING_LEFT+1]=buffer[MAPPING_LEFT+5];
+            cubeArr[MAPPING_LEFT+2]=buffer[MAPPING_LEFT+8];
+            /* CENTER 2 */
+            cubeArr[MAPPING_LEFT+3]=buffer[MAPPING_LEFT+1];
+            cubeArr[MAPPING_LEFT+5]=buffer[MAPPING_LEFT+7];
+            /* BOTTOM 3 */
+            cubeArr[MAPPING_LEFT+6]=buffer[MAPPING_LEFT];
+            cubeArr[MAPPING_LEFT+7]=buffer[MAPPING_LEFT+3];
+            cubeArr[MAPPING_LEFT+8]=buffer[MAPPING_LEFT+6];
             break;
     }
     /* printf("do we free?\n"); */
