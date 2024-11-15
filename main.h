bc2ec04d9c868b531bf23762500b7b46a2766048
Denis Lafin <dslf@live.ru>
2024-11-15 06:18:36 +0300

    direction case and rotateSide

--- main.h
+++ main.h
@@ -8,11 +8,16 @@
 #define MAPPING_FRONT MAPPING_LEFT  + SIZE_OF_SIDE
 #define MAPPING_RIGHT MAPPING_FRONT + SIZE_OF_SIDE
 #define MAPPING_BACK  MAPPING_RIGHT + SIZE_OF_SIDE
-#define MAPPING_DOWN  MAPPING_BACK  + SIZE_OF_SIDE
+#define MAPPING_BOTTOM  MAPPING_BACK  + SIZE_OF_SIDE
 
 #define XDDEBUG 0
 
-
+#define L 0
+#define R 1
+#define F 2
+#define B 3
+#define U 4
+#define D 5
 
 int main(int, char**);
 void drawCube(int *);
@@ -20,3 +25,4 @@ void resetColor();
 void setColor(int n);
 void drawCube__(int *); /* Why __ but not remove? */
 void resetCube(int *);
+void rotateSide(int, int*);
