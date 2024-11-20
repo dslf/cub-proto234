a875ec3b3c53bfbc073aae5bdd125a6efe56d05a
Denis Lafin <dslf@live.ru>
2024-11-20 20:25:36 +0300

    Hello L2

--- c_move_matrix.c
+++ c_move_matrix.c
@@ -37,7 +37,7 @@ void rotateSide(int direction, int *cube_arr, int *buffer_arr) {
 			cube_arr[MAPPING_FRONT+6]=buffer_arr[MAPPING_TOP+6];
 			break;
 
-		case R : 
+		case R: 
 			cube_arr[MAPPING_TOP+2]=buffer_arr[MAPPING_FRONT+2];
 			cube_arr[MAPPING_TOP+5]=buffer_arr[MAPPING_FRONT+5];
 			cube_arr[MAPPING_TOP+8]=buffer_arr[MAPPING_FRONT+8];
@@ -67,7 +67,7 @@ void rotateSide(int direction, int *cube_arr, int *buffer_arr) {
 			cube_arr[MAPPING_RIGHT+8]=buffer_arr[MAPPING_RIGHT+2];
 			break;
 
-		case U :   
+		case U:   
 			cube_arr[MAPPING_TOP]=buffer_arr[MAPPING_TOP+6];
 			cube_arr[MAPPING_TOP+1]=buffer_arr[MAPPING_TOP+3];
 			cube_arr[MAPPING_TOP+2]=buffer_arr[MAPPING_TOP];
@@ -127,7 +127,7 @@ void rotateSide(int direction, int *cube_arr, int *buffer_arr) {
 			cube_arr[MAPPING_RIGHT+6]=buffer_arr[MAPPING_TOP+8];
 			break;
 
-		case B :
+		case B:
 			cube_arr[MAPPING_BACK]=buffer_arr[MAPPING_BACK+6];
 			cube_arr[MAPPING_BACK+1]=buffer_arr[MAPPING_BACK+3];
 			cube_arr[MAPPING_BACK+2]=buffer_arr[MAPPING_BACK];
@@ -157,7 +157,7 @@ void rotateSide(int direction, int *cube_arr, int *buffer_arr) {
 			cube_arr[MAPPING_LEFT]=buffer_arr[MAPPING_TOP+2];
 			break;
 
-		case D :   
+		case D:   
 			cube_arr[MAPPING_BOTTOM]=buffer_arr[MAPPING_BOTTOM+6];
 			cube_arr[MAPPING_BOTTOM+1]=buffer_arr[MAPPING_BOTTOM+3];
 			cube_arr[MAPPING_BOTTOM+2]=buffer_arr[MAPPING_BOTTOM];
@@ -186,5 +186,36 @@ void rotateSide(int direction, int *cube_arr, int *buffer_arr) {
 			cube_arr[MAPPING_RIGHT+7]=buffer_arr[MAPPING_FRONT+7];
 			cube_arr[MAPPING_RIGHT+8]=buffer_arr[MAPPING_FRONT+8];
 			break;
+
+		case L2:
+			cube_arr[MAPPING_LEFT]=buffer_arr[MAPPING_LEFT+8];
+			cube_arr[MAPPING_LEFT+1]=buffer_arr[MAPPING_LEFT+7];
+			cube_arr[MAPPING_LEFT+2]=buffer_arr[MAPPING_LEFT+6];
+
+			cube_arr[MAPPING_LEFT+3]=buffer_arr[MAPPING_LEFT+5];
+			cube_arr[MAPPING_LEFT+4]=buffer_arr[MAPPING_LEFT+4];
+			cube_arr[MAPPING_LEFT+5]=buffer_arr[MAPPING_LEFT+3];
+
+			cube_arr[MAPPING_LEFT+6]=buffer_arr[MAPPING_LEFT+2];
+			cube_arr[MAPPING_LEFT+7]=buffer_arr[MAPPING_LEFT+1];
+			cube_arr[MAPPING_LEFT+8]=buffer_arr[MAPPING_LEFT+0];
+			
+			cube_arr[MAPPING_TOP]=buffer_arr[MAPPING_BOTTOM];
+			cube_arr[MAPPING_TOP+3]=buffer_arr[MAPPING_BOTTOM+3];
+			cube_arr[MAPPING_TOP+6]=buffer_arr[MAPPING_BOTTOM+6];
+			
+			cube_arr[MAPPING_BACK+2]=buffer_arr[MAPPING_FRONT+6];
+			cube_arr[MAPPING_BACK+5]=buffer_arr[MAPPING_FRONT+3];
+			cube_arr[MAPPING_BACK+8]=buffer_arr[MAPPING_FRONT];
+
+			cube_arr[MAPPING_BOTTOM]=buffer_arr[MAPPING_TOP];
+			cube_arr[MAPPING_BOTTOM+3]=buffer_arr[MAPPING_TOP+3];
+			cube_arr[MAPPING_BOTTOM+6]=buffer_arr[MAPPING_TOP+6];
+
+			cube_arr[MAPPING_FRONT]=buffer_arr[MAPPING_BACK+8];
+			cube_arr[MAPPING_FRONT+3]=buffer_arr[MAPPING_BACK+5];
+			cube_arr[MAPPING_FRONT+6]=buffer_arr[MAPPING_BACK+2];
+			break;
+
 	}
 }
